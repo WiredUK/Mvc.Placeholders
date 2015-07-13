79c033c877fbfd6364401553efe2e80cae32a281
David Glass <david@inspiredagency.co.uk>
2015-07-13 15:18:51 +0100

    Added NuGet package

--- Mvc.Placeholders/Properties/AssemblyInfo.cs
+++ Mvc.Placeholders/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Mvc.Placeholders")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Generate placeholder images and text with MVC Html Helpers")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("WiredUK")]
 [assembly: AssemblyProduct("Mvc.Placeholders")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
